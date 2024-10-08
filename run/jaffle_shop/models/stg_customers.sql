759b1478c59dc42e0975586184738a659c770892
Novachevski <82510868+Novachevski@users.noreply.github.com>
2024-10-08 14:15:10 +0000

    Deploying to gh-pages from @ Novachevski/dbt-demo@817a3db75b16446ff0702543f512758f86ba6605 🚀

---
+++ run/jaffle_shop/models/stg_customers.sql
@@ -0,0 +1,15 @@
+
+  
+    
+
+        create or replace transient table analytics.dbt_novachevski.stg_customers
+         as
+        (select
+    id as customer_id,
+    first_name,
+    last_name
+
+from raw.jaffle_shop.customers
+        );
+      
+  
