bf0065f518fc973ad6ed0e8bd97ac27c27bb1d7a
Novachevski <82510868+Novachevski@users.noreply.github.com>
2024-10-10 16:55:59 +0000

    Deploying to gh-pages from @ Novachevski/dbt-demo@8236e5ad719a47d7059de31bb500b3ff58ef92bc 🚀

--- run/jaffle_shop/models/stg_customers.sql
+++ run/jaffle_shop/models/stg_customers.sql
@@ -4,10 +4,7 @@
 
         create or replace transient table analytics.dbt_novachevski.stg_customers
          as
-        (select
-    id as customer_id,
-    first_name,
-    last_name
+        (select id as customer_id, first_name, last_name
 
 from raw.jaffle_shop.customers
         );
